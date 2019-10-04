af5d9325084d487abdb098107726cf18a5c9c1eb
SashoStoichkov <sashostoichkov@gmail.com>
2019-10-04 18:00:50 +0300

    update homework 1

--- 30-09-2019/30-09-2019.c
+++ 30-09-2019/30-09-2019.c
@@ -11,26 +11,10 @@
 //     return result;
 // }
 
-char* my_strcat(char *destination, const char *source){
-    char p[strlen(destination) + strlen(source) + 1];
-    int ptr = 0;
-    int temp = 0;
-
-    while (destination[temp] != '\0'){
-        p[ptr++] = destination[temp++];
-    }
-
-    temp = 0;
-
-    while (source[temp] != '\0'){
-        p[ptr++] = source[temp++];
-    }
-
-    destination = p;
-    
-
-    return destination;
-}
+// char* my_strcat(char *destination, const char *source){
+// 	strcpy(destination + strlen(destination), source);
+// 	return destination;
+// }
 
 // void swap(int *a, int *b){
 //     int temp;
@@ -60,14 +44,52 @@ char* my_strcat(char *destination, const char *source){
 //     return str;
 // }
 
+typedef struct {
+    size_t size;
+    size_t capacity;
+    int *elements;
+} stack;
+
+void stack_init(stack *s, int cap){
+    s = (stack *) malloc (sizeof(stack *));
+    s->size = 0;
+    s->capacity = cap;
+    s->elements = (int *) malloc (sizeof(int) * cap);
+}
+
+void stack_destroy(stack *s){
+    free(s);
+}
+
+int stack_empty(stack *s){
+    if (s->size == 0) return 1;
+    return 0;
+}
+
+void stack_push(stack *s, int num){
+    if (!stack_empty(s)){
+        s->elements[s->size++] = num;
+    }
+}
+
+int stack_top(stack *s){
+    return s->elements[s->size];
+}
+
+void stack_pop(stack *s){
+    if (stack_empty(s)){
+        s->elements[s->size--] = NULL;
+    }
+}
+
 int main(){
 
-    char *name = "Pesho";
-    char *l_name = " Goshev";
+    // char name[100] = "Pesho";
+    // char l_name[100] = " Goshev";
 
     // printf("%d\n", my_strlen(name));
 
-    printf("%s\n", my_strcat(name, l_name));
+    // printf("%s\n", my_strcat(name, l_name));
 
     // int a = 5;
     // int b = 6;
@@ -77,6 +99,20 @@ int main(){
     // printf("%d %d\n", a, b);
 
     // printf("%p\n", my_strdup(name));
+
+    stack *s;
+    stack_init(s, 10);
+
+    for (int i = 0; i < s->capacity; i++){
+        stack_push(s, i+1);
+        printf("Top: %d\n", stack_top(s));
+    }
+
+    for (int i = 0; i < s->size; i++){
+        stack_pop(s);
+    }
+
+    stack_destroy(s);
 
     return 0;
 }
--- 30-09-2019/strcat.c
+++ 30-09-2019/strcat.c
@@ -1,19 +1,4 @@
 char* strcat(char *destination, const char *source){
-    char *p[strlen(destination) + strlen(source) + 1];
-    int ptr = 0;
-    int temp = 0;
-
-    while (destination[temp] != '\0'){
-        p[ptr++] = destination[temp++];
-    }
-
-    temp = 0;
-
-    while (source[temp] != '\0'){
-        p[ptr++] = source[temp++];
-    }
-
-    destination = &p;
-
-    return destination;
+	strcpy(destination + strlen(destination), source);
+	return destination;
 }
